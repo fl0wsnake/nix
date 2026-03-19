80a7a53c22ff1e45d8eeb355d5de1fd15270c7e0
fl0wsnake <lastthursdayist@gmail.com>
2026-03-19 19:35:21 +0200

    .

--- home/.config/nvim/lua/plugins/explorer.lua
+++ home/.config/nvim/lua/plugins/explorer.lua
@@ -1,16 +1,23 @@
 return {
-  'https://github.com/luukvbaal/nnn.nvim',
+  'https://github.com/stevearc/oil.nvim',
   init = function()
-    require("nnn").setup({
-      picker = {
-        style = {
-          width = 1,
-          height = 1
-        }
+    require('oil').setup({ -- https://github.com/stevearc/oil.nvim?tab=readme-ov-file#options
+      delete_to_trash = true,
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          return name:match("^%.%.")
+        end,
+      },
+      keymaps = {
+        ["<C-s>"] = false,
+        ["<C-h>"] = false,
+        ["<C-t>"] = false,
       }
     })
-    vim.keymap.set("n", "<a-x>", function()
-      vim.cmd('NnnPicker %')
-    end, Silent)
-  end
+    vim.keymap.set('', '<A-x>', function() vim.cmd('Oil') end, Silent)
+  end,
+  dependencies = {
+    'https://github.com/nvim-tree/nvim-web-devicons',
+  }
 }
